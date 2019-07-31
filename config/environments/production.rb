5f863e84fb8d2f56e9698a75adc9ec73ea045848
ahmed <ahmedfawzy671998@gmailcom>
2019-07-31 15:46:52 +0200

    fix precompile assets heroku error #2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
    
   # devise says to define default url
-  config.action_mailer.default_url_options = { host: 'saas-project-app-ahmed.herokuapp.com' }
   # config.action_mailer.default_url_options = { :host => 'secure.simple-milia-app.com', :protocol => 'https' }
   #
   # ActionMailer::Base.delivery_method = :smtp
